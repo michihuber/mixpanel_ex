54f4f7a59c4c10f26eaff622395937fc6f717226
Michi Huber <michi.huber@gmail.com>
2014-12-21 18:50:55 +0100

    add hex info

--- mix.exs
+++ mix.exs
@@ -5,7 +5,15 @@ defmodule Mixpanel.Mixfile do
     [app: :mixpanel,
      version: "0.0.1",
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+
+     description: "A client for the Mixpanel HTTP API. See mixpanel.com.",
+     package: [
+       contributors: ["MSch", "michihuber"],
+       licenses: ["MIT"],
+       links: %{"Github" => "https://github.com/michihuber/mixpanel_ex"},
+     ],
+    ]
   end
 
   def application do
@@ -18,3 +26,4 @@ defmodule Mixpanel.Mixfile do
     [{:exjsx, "~> 3.1.0"}]
   end
 end
+
